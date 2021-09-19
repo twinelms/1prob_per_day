30a3d14cd69153251219abb19132e956740636bb
twinelms <86280372+twinelms@users.noreply.github.com>
2021-09-18 21:31:32 -0400

    Update 437.py

--- 437.py
+++ 437.py
@@ -3,12 +3,14 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
         
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         dic = defaultdict(int)
         dic[0] = 1
         self.res = 0
+        
         def findsum(node, cur):
             if not node: return
             cur += node.val
@@ -17,5 +19,6 @@ class Solution:
             findsum(node.left, cur)
             findsum(node.right, cur)
             dic[cur] -= 1  # avoid affecting other subtrees
+            
         findsum(root, 0)
         return self.res
