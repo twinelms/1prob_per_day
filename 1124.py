fa41bcaedcbed36ff8753caa5a5753d72411f687
twinelms <86280372+twinelms@users.noreply.github.com>
2021-12-18 22:39:41 -0500

    Update 1124.py

--- 1124.py
+++ 1124.py
@@ -12,3 +12,19 @@ class Solution:
             if cur not in prefix:
                 prefix[cur] = i
         return res
+    
+class Solution_1:
+    def longestWPI(self, hours: List[int]) -> int:
+        cur = res = 0
+        prefix = {}
+        prefix[0] = -1
+        for i,h in enumerate(hours):
+            cur += 1 if h > 8 else -1
+            if cur > 0:
+                res = max(res, i+1)
+            else:
+                if cur-1 in prefix:
+                    res = max(res, i-prefix[cur-1])
+                if cur not in prefix:
+                    prefix[cur] = i
+        return res
