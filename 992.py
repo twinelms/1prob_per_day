92a3a0d697f9c130a5b3846b402d834ce93bd2e8
twinelms <86280372+twinelms@users.noreply.github.com>
2022-01-17 21:28:53 -0500

    Update 992.py

--- 992.py
+++ 992.py
@@ -1,20 +1,20 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         count = defaultdict(int)
-        i = i0 = j = res = 0
+        i = j = start = res = 0
         while j < len(nums):
             count[nums[j]] += 1
             if count[nums[j]] == 1:
                 k -= 1
             if k < 0:
                 count[nums[i]] -= 1
-                i += 1
-                i0 = i
                 k += 1
+                i += 1
+                start = i
             if k == 0:
-                while count[nums[i]] > 1:
+                while count[nums[i]] != 1:
                     count[nums[i]] -= 1
                     i += 1
-                res += (i-i0)+1
+                res += i-start+1
             j += 1
         return res
