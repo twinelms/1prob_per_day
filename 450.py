cf6988945b4d7029860ef84e9786b75c5af59cbc
twinelms <86280372+twinelms@users.noreply.github.com>
2022-01-17 21:31:10 -0500

    Update 450.py

--- 450.py
+++ 450.py
@@ -1,3 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
@@ -6,14 +12,14 @@ class Solution:
             root.right = self.deleteNode(root.right, key)
         elif root.val > key:
             root.left = self.deleteNode(root.left, key)
+        elif not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
         else:
-            if not root.left:
-                return root.right
-            if not root.right:
-                return root.left
-            rightmost = root.left
-            while rightmost.right:
-                rightmost = rightmost.right
-            root.val = rightmost.val
-            root.left = self.deleteNode(root.left, rightmost.val)
+            smaller = root.left
+            while smaller and smaller.right:
+                smaller = smaller.right
+            root.val = smaller.val
+            root.left = self.deleteNode(root.left, smaller.val)
         return root
